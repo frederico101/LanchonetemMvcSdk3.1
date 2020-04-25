419105985dbf2a0495299c921f2a754e938567c5
frederico101 <fredyalves.fredyalves@hotmail.com>
2020-04-25 14:31:40 -0300

    criando e populando os repositorios com entityFrameWork

---
+++ Migrations/20200424195825_PopularTabela.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lanchonete.Migrations
+{
+    public partial class PopularTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        /*  migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) VALUES('Normal', 'Alface tomate milho batata e uma carne')");
+          migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) VALUES('Natural', 'Alface tomate milho batata')");
+
+         migrationBuilder.Sql(@"INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemUrl, IsLanchePreferido, Preco)
+         Values((SELECT CategoriaId FROM Categorias WHERE CategoriaNome='Normal'),
+         'Pão, hamburger, queijo e batata palha', 'catchup, maionese, palmito',1,
+         'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',
+         'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg', 1 ,12.50)");
+        */
+ 
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Lanche.cs
+++ Models/Lanche.cs
@@ -1,15 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lanchonete.Models
 {
     public class Lanche
     {
         public int LancheId { get; set; }
+        [StringLength(100)]
         public string Nome { get; set; }
+        [StringLength(100)]
         public string DescricaoCurta { get; set; }
+        [StringLength(250)]
         public string DescricaoDetalhada { get; set; }
+        
+        [Column(TypeName="decimal(18,2)")]
         public Decimal Preco { get; set; }
+        [StringLength(300)]
         public string ImagemUrl { get; set; }
+        [StringLength(300)]
         public string ImagemThumbnailUrl { get; set; }
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }
